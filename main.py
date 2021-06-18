64d4f37d66cd916bbbac9772831868d50aeca8ac
Rana Ahmad <taimur1871@gmail.com>
2021-06-17 23:55:19 -0600

    add prediction page

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ created on Tue Jun 15
 # Library imports
 from typing import List
 from fastapi.datastructures import UploadFile
+from fastapi.params import ParamTypes
 
 from wbgapi.source import features
 from fastapi import FastAPI, Request, File, UploadFile, BackgroundTasks
@@ -35,6 +36,9 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 app.mount("/upload", StaticFiles(directory="upload"), name="upload")
 templates = Jinja2Templates(directory="templates/")
 
+# set model upload folder
+model_upload_folder = None
+
 # Welcome page
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request:Request):
@@ -43,8 +47,8 @@ async def read_root(request:Request):
 
 # data properties page
 @app.post("/uploadfile")
-async def read_root(request:Request, background_tasks:BackgroundTasks,
-                    files: List[UploadFile] = File(...)):
+async def upload(request:Request, background_tasks:BackgroundTasks,
+                files: List[UploadFile] = File(...)):
     
     # create an upload folder directory using timestamp
     upload_folder = os.path.join('./upload', time.ctime(time.time()))
@@ -66,22 +70,38 @@ async def read_root(request:Request, background_tasks:BackgroundTasks,
             "message":message})
 
     # open file and get dataframe
-    df, df_head = parse_contents(p, fn)
+    df_train, _ = parse_contents(p, fn)
+    df_pred, _ = parse_contents(p, fn)
 
-    #background_tasks.add_task(mlp_test(df))
+    df_pred_1 = mlp_test(df_train, df_pred, upload_folder)
+    #df_head = df_pred_1.head()
 
     # create a dataframe for displaying data types
-    df_dtypes = pd.DataFrame(df.dtypes)
-    df_dtypes.rename({0:'Data Type'}, axis=1, inplace=True)
+    #df_dtypes = pd.DataFrame(df_train.dtypes)
+    #df_dtypes.rename({0:'Data Type'}, axis=1, inplace=True)
 
-    return templates.TemplateResponse("upload.html", 
-    {"request": request, "dataset_name":p, 
-    "data_summary": [df_dtypes.to_html(classes='data', header='true')],
-    "df_head":[df_head.to_html(classes='data', header='true')]})
+    return templates.TemplateResponse("predictions.html", 
+    {"request": request, "dataset_name":p, "model_path":upload_folder,
+    "data_summary": [df_pred_1.to_html(classes='data', header='true')]})
+
+'''
+# process the dataframe
+@app.post("/predict")
+async def predict_page(request:Request, files: List[UploadFile] = File(...)):
+    # get data from query
+    req_params = request.query_params
+    model_path = req_params.keys()
+
+    return templates.TemplateResponse("predictions.html", 
+    {"request":request, "model_path":model_path})
 
 # process the dataframe
-@app.get("/predictions")
-async def read_root(request:Request):
+@app.get("/predict")
+async def predict_page(request:Request):
+    # get data from query
+    req_params = request.query_params
+    model_path = req_params.keys()
+
     return templates.TemplateResponse("predictions.html", 
-    {"request":request}
-    )
+    {"request":request, "model_path":model_path})
+'''
--- process_data/eda.py
+++ process_data/eda.py
@@ -6,18 +6,16 @@ Initial_EDA.ipynb
 # imports
 import pandas as pd
 import numpy as np
+import pickle
+import os
 
 # import plotting modules
 import seaborn as sns
-import matplotlib.pyplot as plt
 
 # import logistic regression, SVC, random forests, isolation forests?
 # we have target labels so why not see if a model fits on those?
-from sklearn.svm import SVC
-from sklearn.linear_model import LogisticRegressionCV
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier as mlp
-from sklearn.metrics import confusion_matrix, precision_score, recall_score, f1_score
 from sklearn.metrics import classification_report
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import GridSearchCV
@@ -41,6 +39,7 @@ def pair_plot(df):
 
     return pair_plot
 
+# function to process training data
 def pre_process(df):
     
     # combine code 1 and 2 into one category
@@ -67,23 +66,40 @@ def pre_process(df):
 
     return X_train, X_test, y_train, y_test
 
+# function to process prediction data
+def pred_process(df):
+    
+    # combine code 1 and 2 into one category
+    df['category'] = np.where(df['Deduction Code'] == 0, 0,1)
+
+    """### Convert to Dummeis for ML"""
+
+    # get columns and dtypes
+    df.drop('Deduction Code',axis=1, inplace=True)
+
+    # create features and labels
+    X = df[['Building Description', 'Employee Type Description', 'Zip',
+        'Job Start Date', 'Job Pay Rate', 'Yrs Svc']]
+    y = df['category']
+
+    # get dummies for X
+    X_train = pd.get_dummies(X[['Building Description',
+                            'Employee Type Description',
+                            ]])
+
+    return X_train
+
 """### Test MLP"""
 # only setting up MLP for now
-def mlp_test(df):
+def mlp_test(df_train, df_pred, model_save_path):
     # get train and test data
-    X_train, X_test, y_train, y_test = pre_process(df)
+    X_train, X_test, y_train, y_test = pre_process(df_train)
+
+    # get prediction data
+    X_pred = pred_process(df_pred)
 
     # MLP classifier
     mlp_model = mlp(max_iter=1000)
-
-    # fit the data
-    mlp_model.fit(X_train, y_train)
-
-    # get predictions
-    mlp_preds = mlp_model.predict(X_test)
-
-    # get model performance data
-    confusion_matrix(y_test, mlp_preds)
 
     param_grid = [{
         'activation' : ['relu', 'tanh'],
@@ -95,25 +111,40 @@ def mlp_test(df):
     }]
 
     # instantiate GridSearchCV object
-    mlp_clf = GridSearchCV(mlp_model, param_grid, n_jobs=1, cv=5)
+    #mlp_clf = GridSearchCV(mlp_model, param_grid, n_jobs=1, cv=5)
     # fit the classifier
-    mlp_clf.fit(X_train, y_train)
+    #mlp_clf.fit(X_train, y_train)
+    mlp_model.fit(X_train, y_train)
+
+    # make predictions
+    results = mlp_model.predict(X_pred)
+
+    # combine with df_pred
+    df_pred['results'] = results
 
     # view the best hyper-parameters
-    print('optimal hyperparameters:\n', mlp_clf.best_params_)
+    #optimal_hparams = mlp_clf.best_params_
 
     # test model performance using optimized hyper-parameters
-    y_true, y_pred = y_test, mlp_clf.predict(X_test) 
-    print('Results on the test set:\n', classification_report(y_true, y_pred))
+    #y_true, y_pred = y_test, mlp_clf.predict(X_test)
 
-    # get updated predictions
-    mlp_preds = mlp_clf.predict(X_test)
+    # get classification report
+    #results = classification_report(y_true, y_pred)
 
-    # get updated model performance data
-    confusion_matrix(y_test, mlp_preds)
+    # save the model
+    filename = os.path.join(model_save_path, 'saved_model')
+    #pickle.dump(mlp_clf, open(filename, 'wb'))
+    pickle.dump(mlp_model, open(filename, 'wb'))
+
+    return df_pred
 
 # function to get predictions
-def get_preds(df_test, model):
-    # collect samples from df_combined using index from X_test and store in df_
-    df_ = model.predict(df_test)
+def get_preds(df_test, saved_model):
+    # open model
+    loaded_model = pickle.load(open(saved_model, 'rb'))
+    
+    # get results
+    result = loaded_model.predict(df_test)
+
+    return result
     
